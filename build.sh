5c66cad1cd996b44dd1444322732a56b6ff1eac3
nouun <me@nouun.dev>
2022-12-19 17:03:49 +1300

    feat: restructure for multiple keyboard support

--- build.sh
+++ build.sh
@@ -1,21 +1,45 @@
 #! /bin/sh
 
-KEYBOARD="alpha"
-LAYOUT="nouun"
-
-
 shopt -s dotglob
 set -e
 
+usage() {
+  echo "usage: $0 [keyboard] [-fh]" 1>&2 
+}
 
+exit_abnormal() {
+  usage
+  exit 1
+}
+
+[ $# -eq 0 ] && exit_abnormal
+
+KEYBOARD="$1"
+LAYOUT="nouun"
 QMK_ARG="compile"
 
-while getopts 'f' flag; do
+while getopts 'fh' flag  ${@:2}; do
   case "${flag}" in
     f) QMK_ARG="flash" ;;
+    h)
+      usage
+      ;;
+    :)
+      echo "Error: -${OPTARG} requires an argument."
+      exit_abnormal
+      ;;
   esac
 done
 
+if [ "$KEYBOARD" = "" ]; then
+  exit_abnormal
+fi
+
+if [ ! -d "./keyboards/$KEYBOARD" ]; then
+  echo "invalid keyboard: $KEYBOARD"
+  echo "available keyboards: $(ls "./keyboards/")"
+  exit 1
+fi
 
 QMK_HOME=$(qmk config user.qmk_home)
 QMK_HOME=${QMK_HOME#*=}
@@ -34,7 +58,7 @@ QMK_DEST="$QMK_HOME/keyboards/$KEYBOARD/keymaps/$LAYOUT/"
 # Copy layout
 echo " -- Copying new layout"
 mkdir -p "$QMK_DEST"
-cp ./layout/* "$QMK_DEST"
+cp "./keyboards/$KEYBOARD/"* "$QMK_DEST"
 
 # Compile/flash layout
 qmk "$QMK_ARG" -kb "$KEYBOARD" -km "$LAYOUT"
